f854ac8f849b18522a38a49d042d4ae341a98345
William Cai <caimaster1@gmail.com>
2015-11-05 14:03:04 -0500

    passing all homepage tests

--- controllers/index.js
+++ controllers/index.js
@@ -1,12 +1,15 @@
 'use strict';
 
+var events = require('../models/events');
 /**
  * Controller that renders our index (home) page.
  */
 function index (request, response) {
   var contextData = {
     'title': 'MGT 656',
-    'tagline': 'You are doomed (just kidding).'
+    'tagline': 'You are doomed (just kidding).',
+    'events': events.all,
+    'cur_time': new Date(),
   };
   response.render('index.html', contextData);
 }
